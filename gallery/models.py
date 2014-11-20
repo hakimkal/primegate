ef5dc2c1d2b524fd58d3f6213810ad656ef74eb8
Abdulhakim Haliru <hakimkal@gmail.com>
2014-11-20 10:24:39 +0100

    fixed gallery caption to be blank

--- gallery/models.py
+++ gallery/models.py
@@ -10,7 +10,7 @@ class Gallery(models.Model):
     # size is "width x height"
     cropping = ImageRatioField('image', '1600x1066')
     thumb = ImageRatioField('image', '200x200')
-    caption = models.CharField(max_length=200)
+    caption = models.CharField(max_length=200,null=True, blank=True)
     created = models.DateTimeField(default =datetime.datetime.now(), auto_now=False, auto_now_add=True)
     modified = models.DateTimeField(default=datetime.datetime.now(),auto_now=True, auto_now_add=False)
     
